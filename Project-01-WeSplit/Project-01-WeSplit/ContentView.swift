5e9acd0514d3f5605701edb84ac62c4030e901a3
EWANFO LUCKY PETER <peter.ewanfo@gmail.com>
2022-06-20 11:13:22 +0100

    init

---
+++ Project-01-WeSplit/Project-01-WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-01-WeSplit
+//
+//  Created by Ewanfo Lucky Peter on 20/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project-01-WeSplit/Project-01-WeSplit/Project_01_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_01_WeSplitApp.swift
+//  Project-01-WeSplit
+//
+//  Created by Ewanfo Lucky Peter on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Project_01_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
