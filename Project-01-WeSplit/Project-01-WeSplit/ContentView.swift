5f59f8837e6f013311f926a690aa69efa93d61b1
EWANFO LUCKY PETER <peter.ewanfo@gmail.com>
2022-06-21 07:32:45 +0100

    wesplit

--- Project-01-WeSplit/Project-01-WeSplit/ContentView.swift
+++ Project-01-WeSplit/Project-01-WeSplit/ContentView.swift
@@ -9,10 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 20, 30, 40, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = (tipSelection / 100) * checkAmount
+        let grandTotal = checkAmount + tipValue
+        
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
         
-        Text("Hello world")
-            .padding()
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    Picker("Number of People", selection: $numberOfPeople){
+                        ForEach(2..<100 ){number in
+                            Text("\(number) people")
+                        }
+                    }
+                }
+                
+                Section{
+                    Picker("Tip Percentage", selection: $tipPercentage){
+                        ForEach(tipPercentages, id: \.self){
+                            (number) in
+                            Text(number, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section{
+                    Text(totalPerPerson, format:
+                        .currency(code: Locale.current.currencyCode ?? "NGN"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        
+        
+        
         
     
     }
