c4ee381579bc776133e4903a39a54275e6c41597
EWANFO LUCKY PETER <peter.ewanfo@gmail.com>
2022-06-21 16:50:55 +0100

    updated

--- Challenge_1_Day_19/Challenge_1_Day_19/ContentView.swift
+++ Challenge_1_Day_19/Challenge_1_Day_19/ContentView.swift
@@ -7,10 +7,102 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    @State private var inputUnit = ""
+    @State private var outputUnit = ""
+    @State private var inputValue = 0
+    
+    var units = ["Seconds", "Minutes", "Hours", "Days"]
+    
+    var conversionResult : Int{
+        
+        let inputConversionMultiple: Int = getConversionMultiple(inputUnit)
+        let outputConversionMultiple = getConversionMultiple(outputUnit)
+        var _result = 0
+        
+        let inputIsInvalid : Bool = inputUnit.isEmpty || outputUnit.isEmpty
+        
+        if inputUnit != outputUnit && inputIsInvalid == false {
+            let unitToMillseconds = inputConversionMultiple * inputValue
+            
+            _result = unitToMillseconds / outputConversionMultiple
+            
+        }
+        else if inputUnit == outputUnit{
+            _result = inputValue
+        }
+        
+        return _result
+    }
+    
+    func getConversionMultiple(_ unit : String) -> Int{
+        
+        var conversionMultiple = 1
+        
+        switch unit{
+            case "Seconds":
+                conversionMultiple = 1_000
+            case "Minutes":
+                conversionMultiple = 60_000
+            case "Hours":
+                conversionMultiple = 3_600_000
+            case "Days":
+                conversionMultiple = 86_400_000
+            default:
+                conversionMultiple = 1
+        }
+        
+        return conversionMultiple
+        
+    }
+    
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Enter Input value", value: $inputValue, format: .number)
+                        .keyboardType(.numberPad)
+                }header: {
+                    Text("Enter Input Value")
+                }
+                
+                Section{
+                    Picker("Select Input Unit", selection: $inputUnit){
+                        ForEach(units, id: \.self){
+                            unit in
+                            Text("\(unit)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Select Input Unit")
+                }
+                
+                Section{
+                    Picker("Select Output Unit", selection: $outputUnit){
+                        ForEach(units, id: \.self){
+                            unit in
+                            Text("\(unit)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Select Output Unit")
+                }
+                
+                Section{
+                    Text("RESULT =  \(conversionResult) \(outputUnit.lowercased())")
+                }header: {
+                    Text("RESULT")
+                }
+            }
+            .navigationTitle("Unit Conversion")
+            .navigationBarTitleDisplayMode(.automatic)
+        }
     }
 }
 
