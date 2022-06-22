c7389c5a41e82dfdbd0d486860c1f1d23adcc307
EWANFO LUCKY PETER <peter.ewanfo@gmail.com>
2022-06-22 21:53:01 +0100

    guessing the flag predesign

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ewanfo Lucky Peter on 22/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
